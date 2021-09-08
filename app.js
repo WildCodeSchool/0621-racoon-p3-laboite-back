aba76e971d17c9bc7fd465c66ab20a4b6c680088
Axel Gourlin <gourlin.axel@gmail.com>
2021-09-08 11:03:53 +0200

    add config server

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const connection = require('./db-config.js')
+
+const app = express()
+
+const Router = require('./routes/index')
+
+const port = process.env.PORT || 4000
+
+// Connection MySQL
+connection.connect(err => {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+  } else {
+    console.log('connected to database with threadId :  ' + connection.threadId)
+  }
+})
+
+// Route middleware
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded({ extend: true }))
+
+app.use('/url', Router)
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
