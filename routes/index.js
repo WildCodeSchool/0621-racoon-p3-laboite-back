2a3dab750b4c1d3ba823927d5d6dd975add19063
Guigan713 <guillaume.lequin713@gmail.com>
2021-09-16 11:52:55 +0200

    created routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 // Import router: myRouter = require('./myRoute')
-
+const conceptRouter = require('./concept')
 const setupRoutes = app => {
   // add your middware route: app.use('url', myRouter)
+  app.use('/concept', conceptRouter)
 }
 
 module.exports = { setupRoutes }
