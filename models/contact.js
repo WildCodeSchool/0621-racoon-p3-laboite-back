b1f9919f071f11e05bdd2181a5ba276532fd71a7
Axel Gourlin <gourlin.axel@gmail.com>
2021-09-21 10:30:28 +0200

    add table/route/model/index.app.use =>  social

--- models/contact.js
+++ models/contact.js
@@ -4,7 +4,7 @@ const connection = require('../db-config')
 const db = connection.promise()
 
 const getInfo = () => {
-  return db.query('SELECT * FROM contact').then(([results]) => results)
+  return db.query('SELECT * FROM contact').then(([results]) => results[0])
 }
 
 module.exports = { getInfo }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 // Import router: const myRouter = require('./myRoute')
 const conceptRouter = require('./concept')
 const contactRouter = require('./contact')
+const socialRouter = require('./social')
 
 const setupRoutes = app => {
   // add your middware route: app.use('/myUrl', myRouter)
   app.use('/concept', conceptRouter)
   app.use('/contact', contactRouter)
+  app.use('/social', socialRouter)
 }
 
 module.exports = { setupRoutes }
