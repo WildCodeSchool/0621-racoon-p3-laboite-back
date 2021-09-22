69c7024880806f5f64692a00e38a79c4fbe59c94
Guigan713 <guillaume.lequin713@gmail.com>
2021-09-22 09:43:11 +0200

    modified team route and concept route

--- models/team.js
+++ models/team.js
@@ -5,42 +5,42 @@ const db = connection.promise()
 
 
 // Get member team
-const findTeam = () => {
+const getInfo = () => {
   let sql = 'SELECT * FROM team';
   return db.query(sql).then(([results]) => results);
 }
 
-const findOne = (id) => {
-  return db
-    .query('SELECT * FROM movies WHERE id = ?', [id])
-    .then(([results]) => results[0]);
-};
+// const findOne = (id) => {
+//   return db
+//     .query('SELECT * FROM movies WHERE id = ?', [id])
+//     .then(([results]) => results[0]);
+// };
 
-const create = ({ member_img, member_name, member_role }) => {
-  return db
-    .query('INSERT INTO team (member_img, member_name, member_role) VALUES (?, ?, ?)',
-    [member_img, member_name, member_role]
-    )
-    .then(([results]) => {
-      const id = results.insertId;
-      return { id, member_img, member_name, member_role};
-    });
-};
+// const create = ({ member_img, member_name, member_role }) => {
+//   return db
+//     .query('INSERT INTO team (member_img, member_name, member_role) VALUES (?, ?, ?)',
+//     [member_img, member_name, member_role]
+//     )
+//     .then(([results]) => {
+//       const id = results.insertId;
+//       return { id, member_img, member_name, member_role};
+//     });
+// };
 
-const update = (id, newAttributes) => {
-  return db.query('UPDATE team SET ? WHERE id = ?', [newAttributes, id]);
-}
+// const update = (id, newAttributes) => {
+//   return db.query('UPDATE team SET ? WHERE id = ?', [newAttributes, id]);
+// }
 
-const destroy = (id) => {
-  return db
-    .query('DELETE FROM team WHERE id = ?', [id])
-    .then(([result]) => result.affectedRows !== 0);
-}
+// const destroy = (id) => {
+//   return db
+//     .query('DELETE FROM team WHERE id = ?', [id])
+//     .then(([result]) => result.affectedRows !== 0);
+// }
 
 module.exports = {
-  findTeam,
-  findOne,
-  create,
-  update,
-  destroy
+  getInfo,
+  // findOne,
+  // create,
+  // update,
+  // destroy
 }
