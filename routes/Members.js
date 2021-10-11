94a0afb5158694bf0299636203d209eaeb213e17
Axel Gourlin <gourlin.axel@gmail.com>
2021-10-11 10:39:23 +0200

    modify team => member

--- routes/Members.js
+++ routes/Members.js
@@ -1,8 +1,8 @@
-const teamRouter = require('express').Router()
-const Team = require('../models/team')
+const membersRouter = require('express').Router()
+const Member = require('../models/member')
 
-teamRouter.get('/', (req, res) => {
-  Team.getInfo(req.query)
+membersRouter.get('/', (req, res) => {
+  Member.getInfo(req.query)
     .then(team => {
       res.json(team)
     })
@@ -13,9 +13,9 @@ teamRouter.get('/', (req, res) => {
     })
 })
 
-teamRouter.get('/:id', (req, res) => {
+membersRouter.get('/:id', (req, res) => {
   const id = req.params.id
-  Team.findOne(id).then(member => {
+  Member.findOne(id).then(member => {
     if (!member) {
       res.status(404).json({ message: `Member not found` })
     } else {
@@ -24,12 +24,12 @@ teamRouter.get('/:id', (req, res) => {
   })
 })
 
-teamRouter.post('/', (req, res) => {
+membersRouter.post('/', (req, res) => {
   const { member_name } = req.body[0]
   console.log('memName', member_name)
   if (!member_name) res.status(401).json({ message: 'Name is required' })
   else {
-    Team.findOneWithName(member_name).then(user => {
+    Member.findOneWithName(member_name).then(user => {
       console.log('finUser', user)
       if (user) {
         res.status(401).json({ message: `Member already exists` })
@@ -37,7 +37,7 @@ teamRouter.post('/', (req, res) => {
         console.log('body', req.body)
         const { member_img, member_name, member_role } = req.body[0]
         console.log('body data', member_img, member_name, member_role)
-        Team.create(member_img, member_name, member_role)
+        Member.create(member_img, member_name, member_role)
           .then(createdTeam => {
             res
               .status(201)
@@ -52,34 +52,35 @@ teamRouter.post('/', (req, res) => {
   }
 })
 
-// teamRouter.put('/', (req, res) => {
-//   let existingTeam = null;
-//   let validationErrors = null;
-//   Team.findOne(req.params.id)
-//     .then((Team) => {
-//       existingTeam = team;
-//       if (!existingTeam) return Promise.reject('RECORD_NOT_FOUND');
-//       validationErrors = Team.validate(req.body, false);
-//       if (validationErrors) return Promise.reject('INVALID_DATA');
-//       return Team.update(req.params.id, req.body);
-//     })
-//     .then(() => {
-//       res.status(200).json({ ...existingTeam, ...req.body });
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       if (err === 'RECORD_NOT_FOUND')
-//         res.status(404).send(`team with id ${req.params.id} not found.`);
-//       else if (err === 'INVALID_DATA')
-//         res.status(422).json({ validationErrors: validationErrors.details });
-//       else res.status(500).send('Error updating a team.');
-//     });
-// });
+membersRouter.put('/', (req, res) => {
+  const member_id = req.body.member_id
+  let existingTeam = null
+  let validationErrors = null
+  Member.findOne(req.params.id)
+    .then(Team => {
+      existingTeam = Team
+      if (!existingTeam) return Promise.reject('RECORD_NOT_FOUND')
+      validationErrors = Team.validate(req.body, false)
+      if (validationErrors) return Promise.reject('INVALID_DATA')
+      return Team.update(req.params.id, req.body)
+    })
+    .then(() => {
+      res.status(200).json({ ...existingTeam, ...req.body })
+    })
+    .catch(err => {
+      console.error(err)
+      if (err === 'RECORD_NOT_FOUND')
+        res.status(404).send(`team with id ${req.params.id} not found.`)
+      else if (err === 'INVALID_DATA')
+        res.status(422).json({ validationErrors: validationErrors.details })
+      else res.status(500).send('Error updating a team.')
+    })
+})
 
-teamRouter.delete('/:id', (req, res) => {
+membersRouter.delete('/:id', (req, res) => {
   const member_id = req.params.id
   console.log('member_id:', member_id)
-  Team.destroy(member_id)
+  Member.destroy(member_id)
     .then(deleted => {
       if (deleted) res.status(200).json({ message: `🎉 Member deleted!` })
       else res.status(404).json({ message: `Member not found` })
@@ -90,4 +91,4 @@ teamRouter.delete('/:id', (req, res) => {
     })
 })
 
-module.exports = teamRouter
+module.exports = membersRouter
