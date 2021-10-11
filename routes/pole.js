1065a85f41b7f1edafbf7ce3ccce903d23b1b682
fdmesquita <fannydemesquita@gmail.com>
2021-10-11 18:02:02 +0200

    add a get by id in pole/admin without activities

--- routes/pole.js
+++ routes/pole.js
@@ -1,5 +1,5 @@
 const mysql = require('../db-config')
-const { promise } = require('../db-config')
+// const { promise } = require('../db-config')
 const poleRouter = require('express').Router()
 
 // Get all poles without activities
@@ -13,34 +13,17 @@ poleRouter.get('/', (req, res) => {
   })
 })
 
-// Get all activities
-
-poleRouter.get('/', (req, res) => {
-  mysql.query(
-    'SELECT p.*, a.activity_desc, a.activity_img, a.activity_title, a.pole_id, a.id FROM pole as p LEFT JOIN activity as a ON p.id=a.pole_id',
-    (err, result) => {
-      if (err) {
-        res.status(500).send('Error retrieving data from database')
-      } else {
-        res.status(200).json(result)
-      }
+// Get one pole for admin update
+poleRouter.get('/admin/:id', (req, res) => {
+  const poleId = req.params.id
+  mysql.query('SELECT * FROM pole WHERE id = ?', [poleId], (err, result) => {
+    if (err) {
+      res.status(500).send('Error retrieving data from database')
+    } else {
+      res.status(200).json(result)
     }
-  )
+  })
 })
-
-// Get poles with activities
-// poleRouter.get('/', (req, res) => {
-//   mysql.query(
-//     'SELECT p.*, a.activity_desc, a.activity_img, a.activity_title, a.pole_id, a.id FROM pole as p LEFT JOIN activity as a ON p.id=a.pole_id',
-//     (err, result) => {
-//       if (err) {
-//         res.status(500).send('Error retrieving data from database')
-//       } else {
-//         res.status(200).json(result)
-//       }
-//     }
-//   )
-// })
 
 // Get one pole by ID
 poleRouter.get('/:id', (req, res) => {
@@ -91,7 +74,6 @@ poleRouter.get('/:id', (req, res) => {
 })
 
 // Post ---------------------------------------------------------
-
 poleRouter.post('/', (req, res) => {
   const poleData = [
     req.body.pole_name,
@@ -114,25 +96,8 @@ poleRouter.post('/', (req, res) => {
       console.log(err)
       res.status(500).send('Error from database')
     } else {
-      console.log(result)
-      const poleId = result.insertId
-      // const sql2 =
-      //   'INSERT INTO activity (activity_desc, activity_img, activity_title, pole_id) VALUES ?'
-      // const activityData = req.body.activity.map(services => [
-      //   // tableau avec les champs qui attendent les values add [values] in postman
-      //   services[0], //activity_desc
-      //   services[1], //activity_img
-      //   services[2], //activity_title
-      //   poleId
-      // ])
-      // console.log(activityData)
-      // mysql.query(sql2, [activityData], (err, result2) => {
-      //   if (err) {
-      //     res.status(500).send(err)
-      //   } else {
-      //     res.status(201).json({ id: poleId })
-      //   }
-      // })
+      console.table(result)
+      res.status(201).json(result)
     }
   })
 })
@@ -142,6 +107,7 @@ poleRouter.put('/:id', (req, res) => {
   const poleId = req.params.id
   const sql = `UPDATE pole SET ? WHERE id = ?`
   console.log(req.body)
+  console.log(req.params)
   const values = [req.body, poleId]
   mysql.query(sql, values, (err, result) => {
     if (err) {
