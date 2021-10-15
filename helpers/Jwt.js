14f3b203bacade938ec6752585cb718c2274637e
Axel Gourlin <gourlin.axel@gmail.com>
2021-10-15 14:32:48 +0200

    add middlware jwt

---
+++ helpers/Jwt.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken')
+
+const calculateToken = (userEmail = '', user_id = '') => {
+  return jwt.sign(
+    { userEmail: userEmail, user_id: user_id },
+    process.env.PRIVATE_KEY,
+    {
+      expiresIn: '24h'
+    }
+  )
+}
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  console.log('auth', authHeader, 'token', token)
+
+  if (token == null)
+    return res
+      .status(401)
+      .json({ auth: false, message: 'Baerer Json Web Token is required!' })
+
+  jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
+    console.log('decoded:', decoded)
+    if (err)
+      return res.status(401).json({ auth: false, message: 'Wrong Token!' })
+    req.userId = decoded.id
+    next()
+  })
+}
+
+module.exports = { calculateToken, verifyToken }
--- routes/login.js
+++ routes/login.js
@@ -1,39 +1,64 @@
 const loginRouter = require('express').Router()
 const Login = require('../models/login')
 const { hashPassword, verifyPassword } = require('../helpers/crypte')
-const { calculateToken } = require('../helpers/token')
+const { calculateToken, verifyToken } = require('../helpers/Jwt')
 
-loginRouter.post('/addAdmin', async (req, res) => {
+loginRouter.post('/addAdmin', (req, res) => {
   const { email, password } = req.body
-  console.log(req.body)
-  const hashedPassword = await hashPassword(password)
-  const resulst = await Login.addAdmin(email, hashedPassword)
-  if (resulst) res.json({ email, hashedPassword })
+  hashPassword(password)
+    .then(hashedPassword => {
+      Login.addAdmin(email, hashedPassword).then(resulst => {
+        if (resulst) res.json({ email, hashedPassword })
+      })
+    })
+    .catch(error => {
+      res.status(500).json(error.response)
+    })
 })
 
 loginRouter.post('/', (req, res) => {
   const { email, password } = req.body
   let validationErrors = null
   validationErrors = Login.validate(req.body)
-  if (validationErrors) res.status(401).send('Invalid format credentials')
+  if (validationErrors)
+    res.status(401).json({ auth: false, message: 'Invalid format credentials' })
   else {
-    Login.findByEmail(email).then(user => {
-      if (!user) res.status(401).send('Invalid credentials')
-      else {
-        verifyPassword(password, user.admin_password).then(
-          passwordIsCorrect => {
-            if (passwordIsCorrect) {
-              const token = calculateToken(email, user.id)
-              res.header('Access-Control-Expose-Headers', 'x-access-token')
-              res.set('x-access-token', token)
-              res.status(200).send('User connected !')
-            } else {
-              res.status(401).send('Invalid credentials')
+    Login.findByEmail(email)
+      .then(user => {
+        if (!user)
+          res.status(401).json({ auth: false, message: 'Invalid credentials' })
+        else {
+          verifyPassword(password, user.admin_password).then(
+            passwordIsCorrect => {
+              if (passwordIsCorrect) {
+                const accessToken = calculateToken(email, user.id)
+                res.header('Access-Control-Expose-Headers', 'x-access-token')
+                res.set('x-access-token', accessToken)
+                res.status(200).json({
+                  auth: true,
+                  accessToken: accessToken,
+                  result: { id: user.id, email: user.user_email }
+                })
+              } else {
+                res
+                  .status(401)
+                  .json({ auth: false, message: 'Invalid credentials' })
+              }
             }
-          }
-        )
-      }
-    })
+          )
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error.response)
+      })
+  }
+})
+
+loginRouter.get('/isUserAuth', verifyToken, async (req, res) => {
+  try {
+    return res.status(200).json('Your Authenticated!')
+  } catch (error) {
+    return res.status(500).json({ auth: false, message: 'Wrong token' })
   }
 })
 
