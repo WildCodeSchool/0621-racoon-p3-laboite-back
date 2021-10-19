07c3f1878d0bb4e3b70e60a2536cade32a7788a4
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-10-19 09:51:44 +0200

    Member avatar added to member.sql

--- config/member.sql
+++ config/member.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: bac
 -- ------------------------------------------------------
--- Server version	8.0.26
+-- Server version	8.0.25
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,7 +19,6 @@
 -- Table structure for table `member`
 --
 
-DROP TABLE IF EXISTS `team`;
 DROP TABLE IF EXISTS `member`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -30,7 +29,7 @@ CREATE TABLE `member` (
   `member_role` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`member_id`),
   UNIQUE KEY `id_UNIQUE` (`member_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +38,7 @@ CREATE TABLE `member` (
 
 LOCK TABLES `member` WRITE;
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
-INSERT INTO `member` VALUES (1,'sylvie-potter.jpg','Sylvie Vannier','Responsable principal'),(2,NULL,'Thierry Petonnet','Président'),(3,NULL,'Hélène Ferreira','Responsable Pôle Végétal');
+INSERT INTO `member` VALUES (1,'1634579158928bac_female_avatar.png','Sylvie Vannier','Responsable principal'),(2,'1634219953589bac_male_avatar.png','Thierry Petonnet','Président'),(3,'1634219918860bac_female_avatar.png','Hélène Ferreira','Responsable Pôle Végétal');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-13 10:47:13
+-- Dump completed on 2021-10-19  9:50:49
